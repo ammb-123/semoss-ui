bdaebb7e0826121271a703f54ee0d708509a0b95
Baxter <johbaxter@deloitte.com>
2025-02-12 11:12:28 -0500

    fix(client): fix validation on addTeam modal and small style fixes

--- packages/client/src/components/settings/MembersAddOverlay.tsx
+++ packages/client/src/components/settings/MembersAddOverlay.tsx
@@ -225,7 +225,7 @@ export const MembersAddOverlay = (props: MembersAddOverlayProps) => {
                   offset, // offset
                   debouncedSearch ? debouncedSearch : undefined,
               ]
-            : null;
+            : type;
 
     const getMembers = useAPI(open ? getMembersApi : null);
 
