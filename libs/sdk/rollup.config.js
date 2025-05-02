e8bab454bd06186cc69d3efbf0c9d889e02fefe6
Neel <neelneelneel@users.noreply.github.com>
2025-05-02 12:42:34 +0530

    feat: splitting out react as a seperate export

--- libs/sdk/rollup.config.js
+++ libs/sdk/rollup.config.js
@@ -2,30 +2,25 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 
-import bundleSize from "rollup-plugin-bundle-size";
 import postcss from "rollup-plugin-postcss";
 import del from "rollup-plugin-delete";
 import { defineConfig } from "rollup";
 import { terser } from "rollup-plugin-terser";
 
-import packageJson from "./package.json";
+"src/index.ts", "src/js-frameworks/react/index.ts";
 
 export default defineConfig({
-    input: "src/index.ts",
-    output: [
-        {
-            file: packageJson.main,
-            format: "cjs",
-            sourcemap: true,
-            plugins: [terser()],
-        },
-        {
-            file: packageJson.module,
-            format: "esm",
-            sourcemap: true,
-            plugins: [terser()],
-        },
-    ],
+    input: {
+        index: "src/index.ts",
+        react: "src/js-frameworks/react/index.ts",
+    },
+    output: {
+        dir: "dist",
+        format: "esm",
+        sourcemap: true,
+        // plugins: [terser()],
+        entryFileNames: "[name].mjs",
+    },
     plugins: [
         del({ targets: "dist" }),
         resolve(),
@@ -35,7 +30,6 @@ export default defineConfig({
             outputToFilesystem: true,
         }),
         postcss(),
-        bundleSize(),
     ],
     external: ["react", "react-dom"],
 });
